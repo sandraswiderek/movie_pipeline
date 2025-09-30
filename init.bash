578ee00fa2bbd48a08cd24632e22c783684b9c6f
Sandra <sandraswiderekk@gmail.com>
2025-09-30 23:03:56 +0200

    fix

--- init.bash
+++ init.bash
@@ -1,12 +1,25 @@
 PROJECT_ID=data-warehouse-473119
-LOCATION=EU
+PROJECT_NUM=542527165512
+REGION=europe-central2
 TF_STATE_BUCKET=state_bucket_sandi
-CI_SA=github@${PROJECT_ID}.iam.gserviceaccount.com
+CI_SA_NAME=github
+CI_SA=${CI_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com
 RUNTIME_SA=${PROJECT_ID}@appspot.gserviceaccount.com 
+
+
+gcloud iam service-accounts create ${CI_SA} \
+  --display-name=${CI_SA}
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member=${CI_SA} \
+  --role="roles/editor"
+
+gcloud iam service-accounts keys create ./github-key.json \
+  --iam-account=${CI_SA}
 
 gcloud storage buckets create gs://${TF_STATE_BUCKET} \
   --project=${PROJECT_ID} \
-  --location=${LOCATION} \
+  --location=${REGION} \
   --uniform-bucket-level-access
 
 gcloud services enable \
@@ -35,3 +48,9 @@ done
 gcloud iam service-accounts add-iam-policy-binding "$RUNTIME_SA" \
   --member="serviceAccount:${CI_SA}" \
   --role="roles/iam.serviceAccountUser"
+
+
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member="serviceAccount:service-${PROJECT_NUM}@gs-project-accounts.iam.gserviceaccount.com" \
+  --role="roles/pubsub.publisher"
