f327826036130167145992d11faea801f5cbccab
Sandra <sandraswiderekk@gmail.com>
2025-10-01 23:30:23 +0200

    changes and cleanup 2

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -2,6 +2,4 @@ provider "google" {
   project     = var.project_id
   region      = var.region
   credentials = var.credentials_json != "" ? var.credentials_json : null
-  # If credentials_json is empty, provider will use GOOGLE_CREDENTIALS or
-  # Application Default Credentials from the environment.
 }
